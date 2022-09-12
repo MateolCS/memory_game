92c473a7734c73429549704a6bcc30a252c3798e
MateoLCS <mateusz.namysl0318@gmail.com>
2022-09-12 22:36:52 +0200

    Started creating useEffect hook for gettning GoT characters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
 function App() {
+  useEffect(() => {
+    const endpoint = `https://thronesapi.com/api/v2/Characters/`;
+
+    const getCharacters = async () => {
+      const randomIds = new Set();
+      while (randomIds.size < 6) {
+        randomIds.add(Math.floor(Math.random() * 52) + 1);
+      }
+      const fetchedCharacters = await Promise.all(
+        [...randomIds].map((id) =>
+          fetch(`${endpoint}${id}`).then((res) => res.json())
+        )
+      );
+    };
+
+    getCharacters();
+  }, []);
+
   return <div className="App"></div>;
 }
 
