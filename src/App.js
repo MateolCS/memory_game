6b71e6434a789874566f5a9050865ff51274945d
MateoLCS <mateusz.namysl0318@gmail.com>
2022-09-13 22:52:22 +0200

    added some sort of fetching data

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     const endpoint = `https://thronesapi.com/api/v2/Characters/`;
 
     const getCharacters = async () => {
       const randomIds = new Set();
-      while (randomIds.size < 6) {
+      while (randomIds.size < 16) {
         randomIds.add(Math.floor(Math.random() * 52) + 1);
       }
-      const fetchedCharacters = await Promise.all(
-        [...randomIds].map((id) =>
-          fetch(`${endpoint}${id}`).then((res) => res.json())
-        )
+
+      const characterPromises = [...randomIds].map((id) =>
+        fetch(`${endpoint}${id}`)
       );
+
+      const resposnes = await Promise.all(characterPromises);
+
+      setData(await Promise.all(resposnes.map((res) => res.json())));
     };
 
     getCharacters();
   }, []);
+
+  console.log(data);
 
   return <div className="App"></div>;
 }
